267db4141e68d2296a9cb26a33968af3e69d006c
CristianNCC <nicolaecristiancatalin@gmail.com>
2020-11-21 20:31:48 +0200

    Removed obsolete stuff.

--- config.sh
+++ config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo echo -e "\nPROMPT_DIRTRIM=1\n" >> ~/.bashrc
-sudo echo -e "\nalias python=python3" >> ~/.bashrc
-sudo echo -e "\nalias pip=pip3\n" >> ~/.bashrc
-
 # Setup the .bashrc file.
 rm ~/.bashrc
 ln -s ~/repos/dotfiles/.bashrc ~/.bashrc
@@ -16,7 +12,7 @@ ln -s ~/repos/dotfiles/.vimrc ~/.vimrc
 sudo apt-get update
 sudo apt-get upgrade 
 
-# Basic build tools.
+# Basic tools.
 sudo apt-get install build-essential vim git tree
 
 # Cleanup.
