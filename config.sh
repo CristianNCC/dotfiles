45882f5fe6dc7a3231ea80dd272b7ec4368ebfc9
CristianNCC <nicolaecristiancatalin@gmail.com>
2023-07-23 15:04:23 +0300

    Changed .vimrc and added three new vim extensions.

--- config.sh
+++ config.sh
@@ -33,6 +33,13 @@ sudo apt-get install curl tree mlocate iotop nasm bison
 # Cross compiler dependencies
 sudo apt-get install diffutils texinfo flex libgmp-dev
 
+# Vim plugins
+mkdir -pv ~/.vim/pack/git-plugins/start
+cd ~/.vim/pack/git-plugins/start
+git clone --depth=1 https://github.com/bfrg/vim-cpp-modern
+git clone https://github.com/vim-scripts/OmniCppComplete.git
+git clone https://github.com/vim-scripts/DoxygenToolkit.vim.git
+
 ##### Cleanup #####
 sudo apt-get autoremove
 sudo apt-get clean
