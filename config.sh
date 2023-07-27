1b947a37ab61caa218134d5a6665cc327c01a826
CristianNCC <nicolaecristiancatalin@gmail.com>
2023-07-28 02:15:47 +0300

    New minimal .vimrc

--- config.sh
+++ config.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Optional: Vbox guest additions
-# sudo apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-
-###### Setup dotfiles #####
+##### Setup #####
 
 sudo rm ~/.bashrc
 sudo ln -s ~/repos/dotfiles/.bashrc ~/.bashrc
@@ -14,24 +11,16 @@ sudo ln -s ~/repos/dotfiles/.vimrc ~/.vimrc
 sudo rm ~/.aliasrc
 sudo ln -s ~/repos/dotfiles/.aliasrc ~/.aliasrc
 
-##### Update and upgrade #####
-
-sudo apt-get update
-sudo apt-get upgrade 
+sudo apt update
+sudo apt upgrade 
 
 ##### Tools #####
 
-# Basic programming tools.
-sudo apt-get install build-essential vim git
+# Basic programming tools
+sudo apt install build-essential vim git
 
-# Analyis tools.
-sudo apt-get install clang-tools valgrind
-
-# Misc tools.
-sudo apt-get install curl tree mlocate iotop nasm bison 
-
-# Cross compiler dependencies
-sudo apt-get install diffutils texinfo flex libgmp-dev
+# Analyis tools
+sudo apt install clang-tools valgrind
 
 # Vim plugins
 mkdir -pv ~/.vim/pack/git-plugins/start
@@ -41,5 +30,5 @@ git clone https://github.com/vim-scripts/OmniCppComplete.git
 git clone https://github.com/vim-scripts/DoxygenToolkit.vim.git
 
 ##### Cleanup #####
-sudo apt-get autoremove
-sudo apt-get clean
+sudo apt autoremove
+sudo apt clean
