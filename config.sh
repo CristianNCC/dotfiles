44726205262c3f3fdc497cecba9adb0f9ae636cf
CristianNCC <nicolaecristiancatalin@gmail.com>
2023-12-20 19:10:56 +0200

    Removed extra stuff.

--- config.sh
+++ config.sh
@@ -22,12 +22,6 @@ sudo apt install build-essential vim git
 # Analyis tools
 sudo apt install clang-tools valgrind
 
-# Vim plugins
-mkdir -pv ~/.vim/pack/git-plugins/start
-cd ~/.vim/pack/git-plugins/start
-
-git clone --depth=1 https://github.com/bfrg/vim-cpp-modern
-
 ##### Cleanup #####
 sudo apt autoremove
 sudo apt clean
